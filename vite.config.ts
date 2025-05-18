f82dd9a7eba54ee420969288c42534e14d95cd81
tusharkkp <tusharkaldate110@gmail.com>
2025-05-18 19:37:40 +0530

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,6 +3,7 @@ import react from '@vitejs/plugin-react';
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base:'/Visual_Overlay_Map',
   plugins: [react()],
   optimizeDeps: {
     exclude: ['lucide-react'],
